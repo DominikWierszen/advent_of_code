a974d79e9475e9d0ec0d8876475a6587ce7b236f
DominikWierszen <dominikwierszen@gmail.com>
2022-11-12 20:12:01 +0000

    completed day 1 part 1

--- day_1/p1.cpp
+++ day_1/p1.cpp
@@ -1,10 +1,26 @@
+#include <fstream>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 // day1
 int main(int argc, char *argv[]) {
+  ifstream myFile;
   vector<int> data;
-  cout << "Hello World";
+  myFile.open("data.txt");
+  int lines = 0;
+  int count = 0;
+  while (myFile >> lines) {
+    data.push_back(lines);
+  }
+  myFile.close();
+  int previous = data[1];
+  for (int i = 1; i < data.size(); i++) {
+    if (data[i] > data[i - 1]) {
+      count++;
+    }
+  }
+
+  cout << count;
   return 0;
 }
