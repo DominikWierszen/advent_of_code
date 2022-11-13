47e5aff3d2ee575a25c8907c32e7d6b9d14c7fc4
DominikWierszen <dominikwierszen@gmail.com>
2022-11-13 18:32:13 +0000

    progress on day 3

--- day_3/p1.cpp
+++ day_3/p1.cpp
@@ -7,27 +7,25 @@ using namespace std;
 int main(int argc, char *argv[]) {
   ifstream ifile;
   ifile.open("data.txt");
-  string direction;
-  int h_pos = 0, depth = 0, amount;
-  vector<string> directions;
-  vector<int> amounts;
-  while (ifile >> direction >> amount) {
-    directions.push_back(direction);
-    amounts.push_back(amount);
+
+  vector<string> bnums;
+  string bnum;
+  int power_consumption, gamma_rate, epilson_rate, zeroes;
+  while (ifile >> bnum) {
+    bnums.push_back(bnum);
   }
 
-  for (int i = 0; i < directions.size(); i++) {
-    if (directions[i] == "forward") {
-      h_pos = h_pos + amounts[i];
-    } else if (directions[i] == "up") {
-      depth = depth - amounts[i];
-    } else if (directions[i] == "down") {
-      depth = depth + amounts[i];
+  for (string a : bnums) {
+    for (char b : a) {
+      if (b == '0') {
+        zeroes++;
+      }
     }
   }
 
   ifile.close();
-  int answer = depth * h_pos;
-  cout << answer;
+
+  power_consumption = gamma_rate * epilson_rate;
+  cout << power_consumption;
   return 0;
 }
