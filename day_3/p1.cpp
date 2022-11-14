fa5428722afe1f4af74acad2beed38a0da3b8bb9
DominikWierszen <dominikwierszen@gmail.com>
2022-11-14 23:27:03 +0000

    edited d3 p1.cpp

--- day_3/p1.cpp
+++ day_3/p1.cpp
@@ -3,30 +3,18 @@
 #include <vector>
 
 using namespace std;
-
 int main(int argc, char *argv[]) {
   ifstream ifile;
   ifile.open("data.txt");
 
   vector<string> bnums;
   string bnum;
-  // var gamma_rate = "", epilson_rate = "" var zeroes = 0;
   while (ifile >> bnum) {
     bnums.push_back(bnum);
   }
 
-  for (string a : bnums) {
-    for (char b : a) {
-      if (b == '0') {
-        // zeroes++;
-      }
-      // gamma_rate += if (zeroes < a.size() / 2) "0" else "1";
-    }
-  }
-
   ifile.close();
 
-  // power_consumption = gamma_rate * epilson_rate;
-  // cout << power_consumption;
+  // cout <<  gamma_rate * epilson_rate;
   return 0;
 }
