84e21d5e24d850d4414b594f5f1f107b0198fda8
DominikWierszen <dominikwierszen@gmail.com>
2022-11-19 21:21:21 +0000

    s

--- day_3/p2.cpp
+++ day_3/p2.cpp
@@ -6,40 +6,50 @@
 using namespace std;
 int main(int argc, char *argv[]) {
   ifstream ifile;
-  ifile.open("data.txt");
+  ifile.open("example_data.txt");
 
   vector<string> bnums;
   string bnum;
   while (ifile >> bnum) {
     bnums.push_back(bnum);
   }
-  ifile.close();
-  string most_generator_rate = "", co2_scrubber__rate = "";
 
-  vector<string> most;
-  vector<string> least;
-
-  // loops through string
-  while (bnums.size() > 1) {
-    int j = 0;
-    int zeroes = 0;
-    int ones = 0;
-    // loops through strings
-    for (int i = 0; i < bnums.size(); i++) {
-      if (bnums[i].at(j) == *"0") {
-        zeroes++;
-
+  vector<string> data2 = bnums;
+  int i2 = 0;
+  while (data2.size() > 1) {
+    int one = 0;
+    int zero = 0;
+    vector<string> ones = {};
+    vector<string> zeroes = {};
+    for (int i = 0; i < data2.size(); i++) {
+      if (data2[i].at(i2) == '0') {
+        zero++;
+        zeroes.push_back(data2[i]);
       } else {
-        ones++;
+        one++;
+        ones.push_back(data2[i]);
       }
-      if (zeroes > bnums.size() / 2) {
 
-      } else if (zeroes < bnums.size() / 2) {
+      if (zero == one) {
+        data2 = zeroes;
+      } else if (zero > one) {
+        data2 = ones;
       } else {
+        data2 = ones;
       }
     }
-
-    cout << (int)std::stoi(most_generator_rate, nullptr, 2) *
-                (int)std::stoi(co2_scrubber__rate, nullptr, 2);
-    return 0;
+    i2++;
   }
+  cout << data2.size();
+
+  string oxygen = data2[0];
+  cout << oxygen;
+
+  ifile.close();
+  // cout << (int)std::stoi(oxygen, nullptr, 2);
+
+  // answer
+  // cout << (int)std::stoi(oxygen_rate, nullptr, 2) *
+  //             (int)std::stoi(co2_rate, nullptr, 2);
+  return 0;
+}
