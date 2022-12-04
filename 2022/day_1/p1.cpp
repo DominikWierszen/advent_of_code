f7be1abe3168ad6ef0f241a6c66f42aa2cb74e77
DominikWierszen <dominikwierszen@gmail.com>
2022-12-04 21:08:37 +0000

    update

--- 2022/day_1/p1.cpp
+++ 2022/day_1/p1.cpp
@@ -1,14 +1,22 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[]) {
   int answer = 0;
   int calories;
-
+  vector<int> data;
   ifstream myFile;
   myFile.open("data.txt");
+  while (cin >> calories) {
+    if ()
+      data.push_back(calories);
+  }
 
+  for (auto x : data) {
+    cout << x;
+  }
   myFile.close();
   cout << answer;
   return 0;
