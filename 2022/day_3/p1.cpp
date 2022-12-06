b295e1a8ff97d86a1f696ff33ce4ad005e43f53e
DominikWierszen <dominikwierszen@gmail.com>
2022-12-06 22:33:17 +0000

    update

---
+++ 2022/day_3/p1.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  ifstream myFile;
+  myFile.open("data.txt");
+  string word;
+  string half1, half2;
+  vector<vector<string>> halves;
+  unordered_set<char, int> amount;
+
+  while (myFile >> word) {
+    half1 = word.substr(0, word.length() / 2);
+    half2 = word.substr(word.length() / 2);
+    halves.push_back({half1, half2});
+  }
+
+  for (auto x : halves) {
+    for (auto i : x) {
+    }
+
+    amount = {};
+  }
+
+  myFile.close();
+  return 0;
+}
